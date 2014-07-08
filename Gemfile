1dacdd5b42de22c4ea3650e0c362d20758e00d66
Will Lowry <william.c.lowry@gmail.com>
2014-07-08 00:07:04 -0400

    finishes adding bootsrap, partials in views

--- Gemfile
+++ Gemfile
@@ -2,7 +2,9 @@ source 'https://rubygems.org'
 ruby '2.0.0'
 #ruby-gemset=railstutorial_rails_4_0
 
-gem 'rails', '4.0.5'
+gem 'rails', '4.0.8'
+gem 'bootstrap-sass', '2.3.2.0'
+gem 'sprockets', '2.11.0'
 
 group :development, :test do
   gem 'sqlite3', '1.3.8'
@@ -13,7 +15,6 @@ group :test do
   gem 'selenium-webdriver', '2.35.1'
   gem 'capybara', '2.1.0'
 end
-
 gem 'sass-rails', '4.0.1'
 gem 'uglifier', '2.1.1'
 gem 'coffee-rails', '4.0.1'
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,63 +1,61 @@
 require 'spec_helper'
 
 describe "Static pages" do
-	let(:title) {"Ruby on Rails Tutorial Sample App |"}
 
   describe "Home page" do
 
     it "should have the content 'Sample App'" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).to have_content('Sample App')
     end
 
     it "should have the base title" do
-    	visit '/static_pages/home'
-    	expect(page).to have_title("Ruby on Rails Tutorial Sample App")
+      visit root_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App")
     end
 
     it "should not have a custom page title" do
-      visit '/static_pages/home'
+      visit root_path
       expect(page).not_to have_title('| Home')
     end
   end
 
-  describe "Help page" do 
+  describe "Help page" do
 
-  	it "should have the content 'Help'" do 
-  		visit '/static_pages/help'
-  		expect(page).to have_content('Help')
-  	end
+    it "should have the content 'Help'" do
+      visit help_path
+      expect(page).to have_content('Help')
+    end
 
-    it "should have the right title- Help" do
-    	visit '/static_pages/help'
-    	expect(page).to have_title("#{title} Help")
-    end  	
-  end
-
-  describe "About page" do 
-
-  	it "should have the content 'About Us'" do 
-  		visit '/static_pages/about'
-  		expect(page).to have_content('About Us')
-  	end
-
-    it "should have the right title - About Us" do
-    	visit '/static_pages/about'
-    	expect(page).to have_title("#{title} About Us")
+    it "should have the title 'Help'" do
+      visit help_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Help")
     end
   end
 
-  describe "Contact page" do 
+  describe "About page" do
 
-  	it "should have the content 'Contact Us'" do 
-  		visit '/static_pages/contact'
-  	end
+    it "should have the content 'About Us'" do
+      visit about_path
+      expect(page).to have_content('About Us')
+    end
 
-  	it "should have the correct title - 'Contact Us'" do 
-  		visit '/static_pages/contact'
-  		expect(page).to have_title("#{title} Contact Us")
-  	end
+    it "should have the title 'About Us'" do
+      visit about_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | About Us")
+    end
   end
 
+  describe "Contact page" do
 
+    it "should have the content 'Contact'" do
+      visit contact_path
+      expect(page).to have_content('Contact')
+    end
+
+    it "should have the title 'Contact'" do
+      visit contact_path
+      expect(page).to have_title("Ruby on Rails Tutorial Sample App | Contact")
+    end
+  end
 end
