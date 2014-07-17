135d07fed680a038923c505bfc7222f18ed380fe
Will Lowry <william.c.lowry@gmail.com>
2014-07-16 22:32:29 -0400

    all green for authentication

--- spec/requests/authentication_pages_spec.rb
+++ spec/requests/authentication_pages_spec.rb
@@ -4,7 +4,7 @@ describe "Authentication" do
 
   subject { page }
 
-  describe "signin page" do
+  describe "signin" do
     before { visit signin_path }
 
     describe "with invalid information" do
@@ -30,7 +30,7 @@ describe "Authentication" do
     	it { should have_title(user.name) }
     	it { should have_link('Profile', href: user_path(user)) }
     	it { should have_link('Sign out', href: signout_path) }
-    	it { should have_link('Sign in', href: signin_path) }
+    	it { should_not have_link('Sign in', href: signin_path) }
 
   	end
   end
